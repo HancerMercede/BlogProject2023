a8809a4497633eb1c3f494da63b018f07cfbee12
Hancer <hancermercedes@gmail.com>
2023-07-23 21:46:33 -0400

    changing the configuration of the model Comment.js to a sequelize model.

--- src/database/models/comment.js
+++ src/database/models/comment.js
@@ -1,27 +1,41 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Comment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+import { DataTypes, UUIDV4 } from "sequelize";
+import sequelize from "../../Persistence/database";
+
+const Comment = sequelize.define("Comment", {
+  id:{
+    typpe:DataTypes.STRING,
+    primaryKey:true,
+    defaultValue:UUIDV4,
+    allowNull:false,
+    validate:{
+      isUUID:4
     }
-  }
-  Comment.init({
-    id: DataTypes.STRING,
-    idPost: DataTypes.STRING,
-    content: DataTypes.STRING,
-    username: DataTypes.STRING,
-    date: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'Comment',
-  });
-  return Comment;
-};
+  },
+    idPost:{
+      typpe:DataTypes.STRING,
+      allowNull:false,
+      validate:{
+        isUUID:4
+      }
+    },
+    content:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    username:{
+      type:DataTypes.STRING, 
+      allowNull:false,
+      validate:{
+      isEmail:true
+      }
+    },
+    commentDate:{
+      type:DataTypes.DATE,
+      allowNull:false
+      }
+      
+  },{
+    modelName:"Comment",
+    sequelize
+
+});
