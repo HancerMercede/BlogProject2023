5d4bc138cbc55c1e061ce2dbb0a43568b4324059
Hancer <hancermercedes@gmail.com>
2023-07-23 21:25:05 -0400

    changing the implementation to use sequelize Post model.

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,20 +1,21 @@
-import Post from '../models/post.js';
+import Post from '../database/models/post.js';
 
 
 const postService = {
+
     findAll:async(req, res, next) => {
 
-         const posts = await Post.find();
+         const posts = await Post.findAll();
          res.status(200).send(posts);  
     },
 
     findById:async(req,res,next)=>{
       try{
         const {id} = req.params;
-        const post = await Post.findOne({id:id})
-                               .populate('comments')
-                               .exec();
-        
+        const post = await Post.findByPk(id);
+
+        if(!post){ return res.status(404).json({message: 'Post not found'});}
+                                      
         res.status(200).send(post);
        
       }catch(err){
@@ -42,12 +43,8 @@ const postService = {
         const {id} = req.params;
 
         const post = req.body;
-        const updatePost = await Post.findOneAndUpdate({id:id}, post, {
-          upsert:true,
-          new:true
-        });
-        
-        res.status(200).json(updatePost);
+        const updatePost = await Post.update(post, {where: {id:id}});
+        res.status(204).json(updatePost);
 
       }catch(err){
           console.error(err);
@@ -59,7 +56,7 @@ const postService = {
        try{
 
         const {id} = req.params; 
-        const deleted = await Post.deleteOne({id:id});
+        const deleted = await Post.destroy({where:{id:id}})
         res.status(204).json(deleted);
 
        }catch(err){
