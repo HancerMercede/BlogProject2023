499b80227d08de7cbd0af2db35d1f850afc59747
Hancer <hancermercedes@gmail.com>
2023-07-26 12:53:42 -0400

    adding validations if a comment is null or array is empty.

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,60 +1,69 @@
 import Comment from "../database/models/comment.js";
 
-
 const commentService = {
-      
-    findAllCommentsByPost:async(req,res,next)=>{
-       
-        const {id} = req.params;
-        const comments = await Comment.findAll({where: {idPost:id}});
+  findAllCommentsByPost: async (req, res, next) => {
+    const { id } = req.params;
+    const comments = await Comment.findAll({ where: { idPost: id } });
 
-        if(comments === null){return res.send(`No comments yet for this post: ${id}`)}
-        res.status(200).send(comments);
-    },
-
-    findCommentForPostById:async(req,res,next)=>{
-        const {id, idComment} = req.params;
-        const comment = await Comment.findAll({where:{idPost:id, id:idComment}});
-
-        if(comment === null) { return res.send(`No comment with this id : ${idComment}`); }
-
-        res.status(200).send(comment);
-    },
-
-    create:async(req,res,next) => {
-        const {id} = req.params;
-        const model = req.body;
-        const createComment = await Comment.create({username:model.username, date:model.date, content:model.content, idPost:id});
-        res.status(200).send(createComment);
-    },
-
-    updateCommentForPostById:async(req,res, next) => {
-     try{
-
-        const {id, idComment} = req.params;
-        const comment = req.body;
-        const commentUpdated = await Comment.update(comment,{where:{idPost:id, id:idComment}});
-        res.status(200).json(commentUpdated);
-
-     }catch(err){
-        console.error(err);
-        next(err);
-     }
-    },
-
-    delete:async(req,res,next) => {
-       try{
-
-        const {id, idComment} = req.params;
-        const commetDeleted = await Comment.destroy({where:{idPost:id, id:idComment}});
- 
-        res.status(204).send(commetDeleted);
-
-       }catch(err){
-        console.error(err);
-        next(err);
-       }
+    if (comments.length === 0 || comments === null) {
+      return res.send(`No comments yet for this post: ${id}`);
     }
-}
+    res.status(200).send(comments);
+  },
+
+  findCommentForPostById: async (req, res, next) => {
+    const { id, idComment } = req.params;
+    const comment = await Comment.findAll({
+      where: { idPost: id, id: idComment },
+    });
+    console.log(comment);
+    if (comment.length === 0 || comment === null) {
+      return res.send(`No comment with this id : ${idComment}`);
+    }
+
+    res.status(200).json(comment);
+  },
+
+  create: async (req, res, next) => {
+    const { id } = req.params;
+    const model = req.body;
+    const createComment = await Comment.create({
+      username: model.username,
+      date: model.date,
+      content: model.content,
+      idPost: id,
+    });
+    res.status(200).send(createComment);
+  },
+
+  updateCommentForPostById: async (req, res, next) => {
+    try {
+      const { id, idComment } = req.params;
+      const comment = req.body;
+      const commentUpdated = await Comment.update(comment, {
+        where: { idPost: id, id: idComment },
+      });
+
+      res.status(200).json(commentUpdated);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  },
+
+  delete: async (req, res, next) => {
+    try {
+      const { id, idComment } = req.params;
+      const commetDeleted = await Comment.destroy({
+        where: { idPost: id, id: idComment },
+      });
+
+      res.status(204).send(commetDeleted);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  },
+};
 
 export default commentService;
