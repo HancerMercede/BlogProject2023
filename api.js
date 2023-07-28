dea42e49c16cfc8227b124cc4ba10437fc5c178f
Hancer <hancermercedes@gmail.com>
2023-07-28 11:27:18 -0400

    Implementing CORS, (Cross Origin Resource Sharing) in the api in the main time we are open to all clients but later we just going to allow the front-end client to consume this api.

--- api.js
+++ api.js
@@ -1,42 +1,53 @@
-import express from 'express';
-import {Auth, isAuthenticated} from './src/auth/auth.js';
-import _postService from './src/services/post.service.js';
-import _commentService from './src/services/comment.service.js';
-import _postrelService from './src/services/postrel.service.js';
+import express from "express";
+import { Auth, isAuthenticated } from "./src/auth/auth.js";
+import _postService from "./src/services/post.service.js";
+import _commentService from "./src/services/comment.service.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 
-
-app.get('/', (req,res,next)=>{
-    res.send('Hello Node.js');
+app.get("/", (req, res, next) => {
+  res.send("Hello Node.js");
 });
 
-app.post('/auth/register',Auth.register);
-app.post('/auth/login', Auth.login);
+app.post("/auth/register", Auth.register);
+app.post("/auth/login", Auth.login);
 
-app.get('/auth', isAuthenticated, (req, res)=>{
-    res.send(req.user);
+app.get("/auth", isAuthenticated, (req, res) => {
+  res.send(req.user);
 });
 
 //Comments EndPoints
-app.get('/posts', _postService.findAll);
-app.get('/postsToMariaDb', _postrelService.findAllFromMariaDb);
-app.get('/posts/:id', _postService.findById);
-app.post('/posts', isAuthenticated, _postService.create);
-app.put('/posts/:id', isAuthenticated, _postService.update)
-app.delete('/posts/:id', isAuthenticated, _postService.Delete)
+app.get("/posts", _postService.findAll);
+app.get("/posts/:id", _postService.findById);
+app.post("/posts", isAuthenticated, _postService.create);
+app.put("/posts/:id", isAuthenticated, _postService.update);
+app.delete("/posts/:id", isAuthenticated, _postService.Delete);
 
 //Comments EndPoints
-app.get('/posts/:id/comments', _commentService.findAllCommentsByPost);
-app.get('/posts/:id/comments/:idComment', _commentService.findCommentForPostById);
-app.post('/posts/:id/comments', isAuthenticated, _commentService.create);
-app.put('/posts/:id/comments/:idComment', isAuthenticated, _commentService.updateCommentForPostById);
-app.delete('/posts/:id/comments/:idComment', isAuthenticated,_commentService.delete);
+app.get("/posts/:id/comments", _commentService.findAllCommentsByPost);
+app.get(
+  "/posts/:id/comments/:idComment",
+  _commentService.findCommentForPostById
+);
+app.post("/posts/:id/comments", isAuthenticated, _commentService.create);
+app.put(
+  "/posts/:id/comments/:idComment",
+  isAuthenticated,
+  _commentService.updateCommentForPostById
+);
+app.delete(
+  "/posts/:id/comments/:idComment",
+  isAuthenticated,
+  _commentService.delete
+);
 
-
-const server = app.listen(port, ()=> console.log('server running in port:',port));
+const server = app.listen(port, () =>
+  console.log("server running in port:", port)
+);
 server.listen;
